7995cedbcf5c39dccc0d04cbe92ffb528adc1932
Michael Kotthaus <michael.kotthaus@gmx.net>
2021-02-07 23:59:47 +0100

    first try of visualization data on canvas as a bar chart

--- public/sketch.js
+++ public/sketch.js
@@ -1,14 +1,37 @@
 const publicURL = "http://gump500-api.glitch.me"; // Use empty string "" here if you want to fetch data from a local server
+let dataLoaded = false;
+let runners = {};
 
 function setup() {
-  noCanvas();
+  createCanvas(1200, 800);
+  noLoop();
   visualize();
+}
+
+function draw() {
+  background(0,0,128); 
+  textSize(32);
+  fill(255); // actually the text color
+  let message = "";
+  if (! dataLoaded) {
+    let origin = (publicURL.length > 0) ? publicURL : "local machine"; 
+    message = "Fetching data from " + origin + " ...";
+  }
+  else {
+    message = "Gump 500 data fetched";
+  }
+  text(message, 10, 25);
+  for (let i=0; i<runners.length; i++) {
+    textSize(16);
+    text(runners[i].name, 10, 80 + i*25);
+    rect(140, 64 + i*25, runners[i].total*4, 16);
+  }
 }
 
 async function visualize() {
   // Get all runners as json
   const response = await fetch(publicURL + "/api");
-  const runners = await response.json();
+  runners = await response.json();
   console.log(runners);
   // Sort runners from highest total to lowest
   runners.sort((a, b) => b.total - a.total);
@@ -17,4 +40,6 @@ async function visualize() {
   for (let runner of runners) {
     createDiv(`${runner.name} ran a total of ${runner.total} miles.`);
   }
+  dataLoaded = true;
+  redraw();
 }
